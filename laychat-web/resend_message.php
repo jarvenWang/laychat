e83deede79436e1b8a16ccb2c57072c1f2fa7765
王金宝 <1046510934@qq.com>
2017-01-18 14:49:53 +0800

    Revert "init.php resend_message.php send_message.php 中修改PHPSESSID"
    This reverts commit 16325a6473e42f7a3ea810ef3bf71d4d8b0e7b48.

--- laychat-web/resend_message.php
+++ laychat-web/resend_message.php
@@ -8,7 +8,7 @@ use Lib\Db;
 require_once __DIR__ . '/__init.php';
 _session_start();
 if (!isset($_SESSION['laychat'])) {
-    $cook=$_GET['PHPSESSID'];
+    $cook=$_GET['PHPSESSIDA'];
     $db = Db::instance('laychat');
 
     $res = $db->query("select * from `user` where uid ='$cook'");
